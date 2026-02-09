c79304cd165541db94c29427bf4d4ca84fd0f6c0
Roman <romanpieranni@gmail.com>
2026-02-09 11:52:02 +0100

    first steps in js

--- JAVASCRIPT/JAVASCRIPT/app.js
+++ JAVASCRIPT/JAVASCRIPT/app.js
@@ -2,3 +2,11 @@ console.log("Hola Mundo Mundial")
 console.log("ADIOS")
 console.warn("Aviso!")
 console.error("Lo has Roto!!")
+
+
+let nombre= "Anakleto"
+let curso="ASIR"
+console.log("Te llamas: " + nombre, "y Estas en: " + curso)
+
+nombre="Julieta"
+curso="DAW"
