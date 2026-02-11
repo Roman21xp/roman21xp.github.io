c350d4c80b1e28fb7f81e24bc9791d9ebe7d1dd6
Roman <romanpieranni@gmail.com>
2026-02-11 12:11:26 +0100

    let infinito=bucle for(infinito=1000e;infinito>1;infinito++){console.log(infinito)}

--- JAVASCRIPT/JAVASCRIPT/app.js
+++ JAVASCRIPT/JAVASCRIPT/app.js
@@ -1,14 +1,31 @@
 let nombre= "Anakleto"
 let curso="ASIR"
-let edad=
-if (edad > 17) {
-    console.log("puede beber")
-    console.log("puede ir a la discoteca")
-    console.log("puede tomar cafe")
+let edad= 18
+
+
+//if (edad > 17) {
+//    console.log("puede beber")
+//    console.log("puede ir a la discoteca")
+//    console.log("puede tomar cafe")
+//}
+//else {
+//    if (edad > 12) {
+//        console.log("puede beber")
+//    }
+//        console.log("No puede beber")
+//}
+
+//cadena=""
+
+//cadena= edad>17 ? "puede beber." : "No puede beber.";
+//console.log(cadena)
+
+let num=0
+for(num=1;num<10;num++){
+    console.log(num)
 }
-else {
-    if (edad > 12) {
-        console.log("puede beber")
-    }
-        console.log("No puede beber")
-}
+
+// while (num<11) {
+//     num++
+//      console.log(num)
+// }
