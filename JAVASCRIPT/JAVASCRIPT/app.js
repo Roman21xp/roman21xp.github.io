8ccef01becdf1cf742fc2bfdce054f0fa1f6d9b5
Roman <romanpieranni@gmail.com>
2026-02-18 11:59:15 +0100

    javascripto

--- JAVASCRIPT/JAVASCRIPT/app.js
+++ JAVASCRIPT/JAVASCRIPT/app.js
@@ -20,12 +20,52 @@ let edad= 18
 //cadena= edad>17 ? "puede beber." : "No puede beber.";
 //console.log(cadena)
 
-let num=0
-for(num=1;num<10;num++){
-    console.log(num)
-}
+// let num=0
+// for(num=1;num<10;num++){
+//     console.log(num)
+// }
 
 // while (num<11) {
 //     num++
 //      console.log(num)
 // }
+
+/* Imprime un numero aleatorio */
+
+// function dameNumero(max){
+// let numero=Math.floor(Math.
+//     random() * max)
+// console.log(numero)
+// }
+
+// let veces=0
+// while (veces<=10) {
+//     dameNumero(10000001)
+//     veces++
+// }
+
+/* Suma dos numero y devuelve el total */
+function suma(a,b){
+    let total= a+b
+    return total
+}
+
+let misuma=suma(234, 456)
+console.log ("la suma es: " + misuma)
+
+
+function dameNumero(max){
+let numero=Math.floor(Math.
+    random() * max)
+console.log(numero)
+}
+
+let veces=1
+let numeros=[]
+while (veces<=6) {
+    veces++
+    let variable=dameNumero(49)
+    numeros.push(variable)
+}
+
+console.log(numeros)
