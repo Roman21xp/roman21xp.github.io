1b4a0c5ca43f85c64493f9b2129e1a74a789ffeb
Roman <romanpieranni@gmail.com>
2026-02-25 11:50:35 +0100

    consolo.log(ejercicio 4)

--- JAVASCRIPT/ejercicio4.js
+++ JAVASCRIPT/ejercicio4.js
@@ -1,14 +1,20 @@
+//Definimos los caracteres que vamos a usar
 let caracteres = "abcdefghijklmnopqrstuvwxyz";
 let caracteresMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let caracteresNumeros = "0123456789";
 let caracteresEspeciales = "!@#$%^&*()_+|~`-={}[]:;'<>?,./";
 
+// devuelve un valor aleatorio entre min y max
 function numeroAleatorio(min, max) {
 return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+//generar contraseña
+//numeroaleatorio
 let num=numeroAleatorio(1, 195)
-console.log(num)
+//letra especial aleatoria
 console.log(caracteresEspeciales[4])
 
-password = password.split('').sort(() => Math.random() - 0.5).join('');
+let letraEspecial=carateresEspeciales[numeroAleatorio(0,30)]
+
+let password=num+letraEspecial
+console.log(password)
